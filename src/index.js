98734606b3ac3cccc065d34bc873b76f8f2c8381
Torteorte <lipstort@gmail.com>
2021-09-13 21:37:34 +0300

    added 2 modal in header

--- src/index.js
+++ src/index.js
@@ -6,29 +6,31 @@ import './dropDown.js';
 const userAlexLogin = "567@mail.deu";
 const userAlexPassword = "456";
 
-const form  = document.querySelector('#login-form');
+const form = document.querySelector('#login-form');
 const email = document.querySelector('#login-email');
 const password = document.querySelector('#login-password');
 const invalidTextEmail = document.querySelector('.validate-email');
 const invalidTextPassword = document.querySelector('.validate-password');
 
-form.addEventListener('submit', function (event) {
-   event.preventDefault();
-   invalidTextEmail.textContent = '';
-   invalidTextPassword.textContent = '';
+if (form) {
+   form.addEventListener('submit', function (event) {
+      event.preventDefault();
+      invalidTextEmail.textContent = '';
+      invalidTextPassword.textContent = '';
 
-   invalidLogin(event);
+      invalidLogin(event);
 
-   if(!email.validity.valid) {
-      showError();
-   }
-   // document.location.href = './indexUser.html'
-});
+      if (!email.validity.valid) {
+         showError();
+      }
+      // document.location.href = './indexUser.html'
+   });
+}
 
 function showError() {
    if (email.validity.valueMissing) {
       invalidTextEmail.textContent = 'Введите email адрес';
-   } else if(email.validity.typeMismatch) {
+   } else if (email.validity.typeMismatch) {
       invalidTextEmail.textContent = 'Это поле не является email адресом';
    }
 }
@@ -49,4 +51,20 @@ function invalidLogin(event) {
    } else {
       document.location.href = './indexUser.html';
    }
+}
+
+const achievementsTime = document.querySelector("#achievements-time")
+
+let dateNow = new Date()
+achievementsTime.textContent = `Время ${formatDate(dateNow)}`
+
+function formatDate(date) {
+
+   let hours = date.getHours();
+   if (hours < 10) hours = '0' + hours;
+
+   let minutes = date.getMinutes();
+   if (minutes < 10) minutes = '0' + minutes;
+
+   return `${hours}:${minutes}`
 }
